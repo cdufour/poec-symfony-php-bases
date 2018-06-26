7b784141a4b55280b5a7c408bdaba8ca81820739
cdufour <opusidea@gmail.com>
2018-06-26 16:54:38 +0200

    J2 end

--- js/app.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  console.log('app.js chargé');
-  if (Zepto) console.log('Zepto dispo');
-
-  const cbInjured = $('#cbInjured');
-  const rows = $('td.injured').parent();
-  cbInjured.on('click', () => rows.toggle());
-
-})()
